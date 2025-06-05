da684abf4a5b79f45a5a9ae0176061252258f2e0
jaycho0119 <jaycho0119@gmail.com>
2025-06-05 15:50:59 +0900

    Update 00_hello.py

--- pages/00_hello.py
+++ pages/00_hello.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.image"(https://giphy.com/gifs/excited-family-guy-stewie-griffin-7eAvzJ0SBBzHy)"
+st.image("https://giphy.com/gifs/excited-family-guy-stewie-griffin-7eAvzJ0SBBzHy")
